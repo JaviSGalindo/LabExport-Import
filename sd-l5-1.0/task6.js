f5de1fa92e1b420e4240d3293133f94b105c0534
JaviSGalindo <javier2006galindo@gmail.com>
2024-12-11 11:29:27 -0500

    Uptade - 10 test passed

--- sd-l5-1.0/task6.js
+++ sd-l5-1.0/task6.js
@@ -1,3 +1,22 @@
-export function rubricExcellent() {
+// 6. A teacher also wants to mark students who get a high score of 9 or more.
+//     * Extend the function so that it returns "Excellent" for scores greater than 8.
 
+
+export function rubricExcellent(score) {
+    if(score<=11 && score>=0){
+        if(score>8){
+            return 'Excellent'
+        }
+        else if(score>=5){
+            return 'Pass'
+        }
+        else{
+            return 'Fail'
+        }
+    }
+    else{
+        return 'Score out of range'
+    }
 }
+const scoreExcellent = rubricExcellent(process.argv[3])
+console.log(scoreExcellent)
--- sd-l5-1.0/task7.js
+++ sd-l5-1.0/task7.js
@@ -1,3 +1,24 @@
-export function rubricPerfect() {
+// 7. A teacher also wants to mark students who get a perfect score of 11.
+//     * Extend the function so that it returns "Perfect" for a score of 11.
 
+export function rubricPerfect(score) {
+    if(score<=11 && score>=0){
+        if(score == 11){
+            return 'Perfect'
+        }
+        else if(score>8){
+            return 'Excellent'
+        }
+        else if(score>=5){
+            return 'Pass'
+        }
+        else{
+            return 'Fail'
+        }
+    }
+    else{
+        return 'Score out of range'
+    }
 }
+const scorePerfect = rubricPerfect(process.argv[3])
+console.log(scorePerfect)
