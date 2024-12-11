8f7de04aecfe546074b671381a842c728be1519c
JaviSGalindo <javier2006galindo@gmail.com>
2024-12-11 11:20:01 -0500

    Uptade - 8 test passed

--- sd-l5-1.0/task5.js
+++ sd-l5-1.0/task5.js
@@ -1,3 +1,19 @@
-export function rubricPassFail() {
+// 5. A teacher wants to create a rubric for grading students based on a score from 0 to 11.
+//     * A student passes if they have a score greater than or equal to 5. Export a function that returns "Pass" or "Fail".
 
+export function rubricPassFail(score) {
+    if(score<=11 && score>=0){
+        if(score>=5){
+            return 'Pass'
+        }
+        else{
+            return 'Fail'
+        }
+    }
+    else{
+        return 'Score out of range'
+    }
+    
 }
+const scoreAprobbed = rubricPassFail(process.argv[3])
+console.log(scoreAprobbed)
